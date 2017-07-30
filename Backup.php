f0ec0a6a0ffcdfb0f70abfb495be4e474d3c66f0
GplCart <gplcart.software@gmail.com>
2017-07-31 00:27:12 +0300

    Minor changes

--- Backup.php
+++ Backup.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * @package Backup 
- * @author Iurii Makukh <gplcart.software@gmail.com> 
- * @copyright Copyright (c) 2017, Iurii Makukh <gplcart.software@gmail.com> 
- * @license https://www.gnu.org/licenses/gpl-3.0.en.html GPL-3.0+ 
+ * @package Backup
+ * @author Iurii Makukh <gplcart.software@gmail.com>
+ * @copyright Copyright (c) 2017, Iurii Makukh <gplcart.software@gmail.com>
+ * @license https://www.gnu.org/licenses/gpl-3.0.en.html GPL-3.0+
  */
 
 namespace gplcart\modules\backup;
